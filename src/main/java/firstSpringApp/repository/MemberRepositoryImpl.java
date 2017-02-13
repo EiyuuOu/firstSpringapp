6cae318a8ddf760d5f5390cf9004416f49fba379
Nikola Bradas <nikola.bradas@fdmgroup.com>
2017-02-13 17:29:34 +0000

    last commit

--- src/main/java/firstSpringApp/repository/MemberRepositoryImpl.java
+++ src/main/java/firstSpringApp/repository/MemberRepositoryImpl.java
@@ -1,9 +1,12 @@
 package firstSpringApp.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import firstSpringApp.model.Member;
 
@@ -22,21 +25,38 @@ public class MemberRepositoryImpl implements MemberRepository {
 	 */
 	@Override
 	public List<Member> getMembers(){
-		Member member1 = new Member("Niko","Brah", "niko@bla.de", "abc123");
-		Member member2 = new Member("John","Rambo", "john@bla.de", "abc123");
-		Member member3 = new Member("James","Bond", "james@bla.de", "abc123");
-		
-		members.add(member1);
-		members.add(member2);
-		members.add(member3);
+//		Member member1 = new Member("Niko","Brah", "niko@bla.de", "abc123");
+//		Member member2 = new Member("John","Rambo", "john@bla.de", "abc123");
+//		Member member3 = new Member("James","Bond", "james@bla.de", "abc123");
+//		
+//		members.add(member1);
+//		members.add(member2);
+//		members.add(member3);
+		String sql = "select * from member";
+		this.jdbcTemplate.query(sql, new MemberMapper());
 		
 		return members;
 	}
 
 	@Override
 	public void addMember(Member newMember) {
-		String sql = "insert into member(id,name,surname,email,password) values(memberseq.nextval,?,?,?,?)";
-		this.jdbcTemplate.update(sql, newMember.getName(), newMember.getSurname(), newMember.getEmail(), newMember.getPassword());
+		String sql = "insert into member(id,name,surname,email,password,registered) values(memberseq.nextval,?,?,?,?,?)";
+		this.jdbcTemplate.update(sql, newMember.getName(), newMember.getSurname(), newMember.getEmail(), newMember.getPassword(), newMember.getRegistered());
 		
 	}
+}
+
+class MemberMapper implements RowMapper<Member>{
+
+	@Override
+	public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+		String name = rs.getString("name");
+		String surname = rs.getString("surname");
+		String email = rs.getString("email");
+		String password = rs.getString("password");
+		
+		return new Member(name, surname, email, password);	
+	}
+	
 }
--- src/main/java/firstSpringApp/view/Main.java
+++ src/main/java/firstSpringApp/view/Main.java
@@ -25,14 +25,18 @@ public class Main {
 		//call from service
 		MemberService service = context.getBean(MemberService.class);
 		
-		Member member = new Member("John", "Rambo", "blue@light.com", "654321");
-		service.add(member);
+//		Member member1 = new Member("John", "Rambo", "blue@light.com", "654321");
+//		Member member2 = new Member("Donald", "Trump", "don@us.gov", "321456");
+//		Member member3 = new Member("Arnold", "Schwarzenegger", "termi@na.tor", "0111010");
+//		service.add(member1);
+//		service.add(member2);
+//		service.add(member3);
 		
-//		List<Member> members = service.getMembers();
-//		
-//		for (Member member : members) {
-//			System.out.println(member);
-//		}
+		List<Member> members = service.getMembers();
+		
+		for (Member member : members) {
+			System.out.println(member);
+		}
 		
 		System.out.println();
 		ProductRepository repo1 = context.getBean("repo1",ProductRepository.class);
